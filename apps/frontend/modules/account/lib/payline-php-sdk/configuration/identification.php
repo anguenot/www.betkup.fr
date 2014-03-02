b4e4a3be883731d5e13f6494c99f1b8de4325ec2
janguenot <janguenot@iland.com>
2014-03-02 08:39:40 -0600

    Moving to github

--- apps/frontend/modules/account/lib/payline-php-sdk/configuration/identification.php
+++ apps/frontend/modules/account/lib/payline-php-sdk/configuration/identification.php
@@ -1,6 +1,6 @@
 <?php
-	DEFINE( 'MERCHANT_ID', '12156100549557' ); // Merchant ID
-	DEFINE( 'ACCESS_KEY', '4CxFvOvxzUQ73AFut4AJ' ); // Certificate key
+	DEFINE( 'MERCHANT_ID', 'xxxxxxxxx' ); // Merchant ID
+	DEFINE( 'ACCESS_KEY', 'xxxxxx' ); // Certificate key
 
 	DEFINE( 'PROXY_HOST', null); // Proxy URL (optional)
 	DEFINE( 'PROXY_PORT', null); // Proxy port number without 'quotes' (optional)
